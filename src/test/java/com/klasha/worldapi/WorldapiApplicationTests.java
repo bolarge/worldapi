d0671cbd89f41d1d5bf1487d4daf2ea0d3001056
Bolaji <bolaji.salau@gmail.com>
2023-07-29 13:52:36 +0100

    Initial setup

---
+++ src/test/java/com/klasha/worldapi/WorldapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klasha.worldapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
