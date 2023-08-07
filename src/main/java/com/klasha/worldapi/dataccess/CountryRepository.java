8cc022c63bca6c337f13bb0f6be065f5cee7712c
Bolaji <bolaji.salau@gmail.com>
2023-08-07 17:03:14 +0100

    Update to country repository and cleaned up

--- src/main/java/com/klasha/worldapi/dataccess/CountryRepository.java
+++ src/main/java/com/klasha/worldapi/dataccess/CountryRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface CountryRepository extends JpaRepository<Country, Integer> {
     Country findCountryByName(String countryName);
     Country findCountryByCurrencyCode(String currencyCode);
-    @Query(value = "SELECT c.id as id, c.name as countryName, c.population as popuation, c.capital_city as capitalCity," +
-            " FROM world_db.city c JOIN world_db.state s ON c.id = s.id JOIN world_db.country c2 ON c2.id = s.id  " +
+    @Query(value = "SELECT c.id as id, c.name as cityName, c.population as popuation, c.city_code as cityCode" +
+            " FROM world_db.city c JOIN world_db.state s ON c.state_id = s.id JOIN world_db.country c2 ON c2.id = s.country_id  " +
             "ORDER BY c.population DESC LIMIT :numberOfCities", nativeQuery = true)
     List<Country> findTopCitiesByPopulation(@Param("numberOfCities") int numberOfCities);
 
